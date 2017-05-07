221d625c3ac62d5f62616f99bc9ffd3e06575521
matej990 <matejkupecky@gmail.com>
2017-05-07 19:19:35 +0200

    Zmenenie štýlu

--- application/views/Taxikari/delete_taxikari.php
+++ application/views/Taxikari/delete_taxikari.php
@@ -1,6 +1,27 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 ?>
-<header class="intro-header" >
+<head>
+ <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+ </head>
+ 
+ <header>
+<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link href="../assets/css/landing-page.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="../assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
+
+<body class="intro-header" >
 <div class="container">
 <br></br>
 <br></br>
@@ -11,7 +32,7 @@
                     <h3 class="panel-title">Pozor</h3>
                 </div>
                 <div class="panel-body">
-                    <h4>Naozaj chcete vymazať vybraný záznam?</h4>
+                    <h4><font color="red">Naozaj chcete vymazať vybraný záznam?</font></h4>
                     <div class="col-md-6 center">
                     <a href="<?php echo base_url('Taxikari'); ?>" class="btn btn-default btn-lg">Zrušiť</a>   
 					</div>
@@ -27,5 +48,5 @@
             </div>
         </div>
     </div>
-<!-- /.container -->
+</body><!-- /.container -->
 </header>
--- application/views/Taxikari/edit_taxikari.php
+++ application/views/Taxikari/edit_taxikari.php
@@ -21,9 +21,9 @@
     <link href="../assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic" rel="stylesheet" type="text/css">
 
-<header class="intro-header" >
+<body class="intro-header" >
 <div class="container">
-
+<body>
         
                
                     
@@ -34,43 +34,40 @@
                 
                  <?php echo validation_errors(); ?>
                 <form action="<?php echo base_url('Taxikari/editing_taxikar'); ?>" method="post" accept-charset="utf-8">
-                <div class="form-group col-lg-4">
+                
                  <?php echo form_hidden('idTaxikari',$id);
                         echo form_label('Meno: ');
                         echo form_input('Meno', $query['Meno'], 'class="form-control" maxlength="30"');
                   ?>
-                </div>
-                <div class="clearfix"></div>
-				<div class="form-group col-lg-4">
-                  <?php echo form_hidden('idTaxikari',$id);
+                
+                
+	                  <?php echo form_hidden('idTaxikari',$id);
                         echo form_label('Priezvisko: ');
                         echo form_input('Priezvisko', $query['Priezvisko'], 'class="form-control" maxlength="30"');
                   ?>
-                </div>
-                <div class="clearfix"></div>
-                <div class="form-group col-lg-4">
+                
+                
+                
                   <?php echo form_hidden('idTaxikari',$id);
                         echo form_label('Auto: ');
                         echo form_input('auto', $query['auto'], 'class="form-control" maxlength="10"');
                   ?>
-                </div>
-                <div class="clearfix"></div>
-                <div class="form-group col-lg-4">
+                
+                
+                
                   <?php echo form_hidden('idTaxikari',$id);
                         echo form_label('Telefónne číslo: ');
                         echo form_input('tel', $query['tel'], 'class="form-control" maxlength="10"');
                   ?>
-                </div>
-                <div class="clearfix"></div>
                 
+                
+                <br>
+				<br>
                 <button class="btn btn-danger btn-circle">
                 
 				<a href="<?php echo base_url('Taxikari'); ?>" >Späť</a> </button>                <input type="submit" name="<?php echo base_url('Taxikari/editing_taxikar'); ?>" value="Uložiť"  class="btn btn-success" />
                 </form>                
             </div>
-        </div>
-
-</div>
-<!-- /.container -->
-
+        <!-- /.container -->
+</body>
 </header>
